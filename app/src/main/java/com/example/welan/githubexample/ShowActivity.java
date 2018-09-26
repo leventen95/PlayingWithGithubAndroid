6b8b704eb2dffb76c82de49f20cfe382ab1e4202
Levente <leventen@hiof.no>
2018-09-26 15:50:26 +0200

    Merge

---
+++ app/src/main/java/com/example/welan/githubexample/ShowActivity.java
@@ -0,0 +1,14 @@
+package com.example.welan.githubexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+    }
+}
